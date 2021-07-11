5b26db38416723799fb5e0bcddaac929ebb5ef4b
Tatyana Tsareva <tatyanac@inbox.ru>
2021-07-11 12:53:56 +0300

    Added Ex15, Lesson 4

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,8 +1,16 @@
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
+import pytest
 
 class TestUserRegister(BaseCase):
+    exclude_params = [
+        ('email'),
+        ('password'),
+        ('username'),
+        ('firstName'),
+        ('lastName')
+    ]
 
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
@@ -20,3 +28,58 @@ class TestUserRegister(BaseCase):
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+    #Ex15: Тесты на метод user
+    # 1. Создание пользователя с некорректным email - без символа @
+    def test_create_user_with_incorrect_email(self):
+        email = 'testemail.com'
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Invalid email format"
+
+    #Ex15: Тесты на метод user
+    # 2. Создание пользователя без указания одного из полей - с помощью дата-провайдера необходимо проверить,
+    # что отсутствие любого параметра не дает зарегистрировать пользователя
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_create_user_without_main_parameter(self, condition):
+        data = self.prepare_registration_data()
+
+        if condition == 'email':
+            del data['email']
+        elif condition == 'password':
+            del data['password']
+        elif condition == 'username':
+            del data['username']
+        elif condition == 'firstName':
+            del data['firstName']
+        elif condition == 'lastName':
+            del data['lastName']
+
+        response = MyRequests.post("/user", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The following required params are missed: {condition}"
+
+    #Ex15: Тесты на метод user
+    # 3. Создание пользователя с очень коротким именем в один символ
+    def test_create_user_with_short_name(self):
+        data = self.prepare_registration_data()
+        data['username'] = 'a'
+
+        response = MyRequests.post("/user", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The value of 'username' field is too short"
+
+    #Ex15: Тесты на метод user
+    # 4. Создание пользователя с очень длинным именем - длиннее 250 символов
+    def test_create_user_with_long_name(self):
+        data = self.prepare_registration_data()
+        data['username'] = 'a'*256
+
+        response = MyRequests.post("/user", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The value of 'username' field is too long"
