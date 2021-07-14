ab88a7e3b0b5e5116c2efadfd824af0464f1a900
Tatyana Tsareva <tatyanac@inbox.ru>
2021-07-14 19:37:34 +0300

    Added Ex16, Lesson 4

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -31,3 +31,31 @@ class TestUserGet(BaseCase):
 
         expected_fields = ['username', 'email', 'firstName', 'lastName']
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    #Ex16: Запрос данных другого пользователя
+    def test_get_user_details_auth_as_another_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        print(user_id_from_auth_method)
+
+        response2 = MyRequests.get(
+            "/user/1",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
+
+
